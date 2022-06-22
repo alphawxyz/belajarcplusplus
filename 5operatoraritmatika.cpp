d60ed058bfa063894226c4459c7eef0426f4c1fc
alphawxyz <105125029+alphawxyz@users.noreply.github.com>
2022-06-22 07:50:26 +0700

    Update 5operatoraritmatika.cpp

--- 5operatoraritmatika.cpp
+++ 5operatoraritmatika.cpp
@@ -1,40 +1,28 @@
 #include<iostream>
-using namespace std; 
+using namespace std;
 
-extern int luasbk,d1,d2;
-int alas;
+int main()
+{
+    int a,b,c; 
+    a = 8;
+    b = 10;
+    cout<<"OPERATOR ARITMATIKA"<<endl;
+    cout<<"Terdiri dari + - * / % ++ --"<<endl;
+    cout<<"A = 8, B = 10"<<endl;
+    c = a+b; 
+    cout<<"\nA + B    : "<<c<<endl;
+    c = a-b; 
+    cout<<"A - B    : "<<c<<endl;
+    c = a*b; 
+    cout<<"A * B    : "<<c<<endl;
+    c = a/b; 
+    cout<<"A / B    : "<<c<<endl;
+    c = a%b; 
+    cout<<"A % B    : "<<c<<endl;
+    c = a++;
+    cout<<"A++      : "<<c<<endl;
+    c = a--;
+    cout<<"A--      : "<<c<<endl;   
 
-#define a 5
-#define t 7
-
-int main(){
-    int luasbk,d1,d2;
-    d1 = 12;
-    d2 = 14;
-    luasbk = d1*d2/2;
-    cout<<"Luas Belah Ketupat       : "<<luasbk<<endl;
-    
-    int luaspp,tinggi;
-    alas = 3;
-    tinggi = 6;
-    luaspp = alas*tinggi;
-    cout<<"Luas Persegi Panjang     : "<<luaspp<<endl;
-    
-    int luasp;
-    int sisi = 4;
-    luasp = sisi*sisi;
-    cout<<"Luas Persegi             : "<<luasp<<endl;
-    
-    int luasjj;
-    luasjj = a*t;
-    cout<<"Luas Jajargenjang        : "<<luasjj<<endl;
-    
-    int luasll;
-    const int p = 12;
-    const int l = 6;
-    luasll = 2*p*l;
-    cout<<"Luas Layang - layang     : "<<luasll<<endl;
-    
     return 0;
-    
 }
